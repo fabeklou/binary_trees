364ad79980ab6c44c8b3ec403575d280a8e6ee18
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-03 02:23:37 +0000

    feat: find the sibling of a node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ *
+ * @node:  a pointer to the node to find the sibling
+ *
+ * Return: a pointer to the sibing node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return (node->parent->left == node ? node->parent->right
+									   : node->parent->left);
+}
