42c9c5970ab53eed18ee25261f3353312be54003
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-03 02:52:41 +0000

    fix: handle different tree depth case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,6 +43,25 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: a pointer to the node to measure the depth
+ *
+ * Return: 0 if tree is NULL, otherwise the depth of the tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t node_depth;
+
+	if (tree == NULL)
+		return (0);
+
+	for (node_depth = 0; tree->parent != NULL; node_depth++)
+		tree = tree->parent;
+	return (node_depth);
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is full
  *
  * @tree: a pointer to the root node of the tree to check
@@ -63,6 +82,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_is_full(tree) == 0)
+		return (0);
+
+	if (binary_tree_depth(tree->left) != binary_tree_depth(tree->right))
 		return (0);
 
 	return (1);
